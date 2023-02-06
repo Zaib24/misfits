fb379ef09c31497c5fc15794e3a029fca1a94b37
ZayB <Zaib.sajid24@gmail.com>
2023-02-06 18:29:56 +0500

    Chapter 2 (Products Details)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
+import data from './Data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Misfits</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  style={{ height: '20rem' }}
+                />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
